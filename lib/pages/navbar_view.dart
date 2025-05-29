472618c25c9234a7387b71f3473244fa42a25b42
amzee03 <2003amirhamzah@gmail.com>
2025-05-29 18:37:31 +0700

    splash screen & home

---
+++ lib/pages/navbar_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class NavbarView extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  const NavbarView({
+    Key? key,
+    required this.selectedIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: selectedIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_outlined),
+          label: 'Kontribusi',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bookmark_border),
+          label: 'Disimpan',
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/pages/splashscreen_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'home_view.dart'; 
+
+class SplashScreenView extends StatefulWidget {
+  const SplashScreenView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
+
+class _SplashScreenViewState extends State<SplashScreenView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomeView()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFD0EDF5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/tagit_logo.png', width: 150, height: 150),
+            const SizedBox(height: 50),
+            const Column(
+              children: [
+                Text('Loading...',
+                    style: TextStyle(fontSize: 18, color: Colors.black87)),
+                SizedBox(height: 10),
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.redAccent),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
